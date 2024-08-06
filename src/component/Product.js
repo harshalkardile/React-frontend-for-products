17b986ed009b36e5b8f4d43a5cb844da917feddb
harshal <kardileharshal567@gmail.com>
2024-08-06 15:12:55 +0530

    products commit

---
+++ src/component/Product.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Container, Card, Button } from "react-bootstrap";
+
+const Product = (props) => {
+
+    console.log(props.match.params.id);
+  return (
+    <Container className="mt-5">
+      
+    </Container>
+  );
+};
+
+export default Product;
---
+++ src/component/Products.js
@@ -0,0 +1,78 @@
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { baseurl } from "./BaseUrl";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+const Products = () => {
+  const navigate = useNavigate();
+  const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const getProducts = async () => {
+      try {
+        let result = await fetch(`${baseurl}/products/`);
+        result = await result.json();
+        console.log(result);
+        setProducts(result);
+        setIsLoading(false);
+      } catch (error) {
+        console.error("Failed to fetch products:", error);
+        setIsLoading(false);
+      }
+    };
+
+    getProducts();
+  }, []);
+
+    function updateProduct(productId) {
+        console.log(productId);
+    navigate(`/products/${productId}`);
+  }
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <Container>
+      <Row>
+        {products.length !== 0 ? (
+          products.products.map((item) => (
+            <Col key={item._id} xs={12} sm={6} md={4} lg={3} className="mb-4">
+              <Card>
+                <Card.Img
+                  variant="top"
+                  src={item.images[0] || "holder.js/100px180"}
+                  alt={item.title}
+                />
+                <Card.Body>
+                  <Card.Title>{item.title}</Card.Title>
+                  <Card.Text>{item.description}</Card.Text>
+                  <Card.Text>{item.category}</Card.Text>
+                  <Card.Text>Price: ${item.price.toFixed(2)}</Card.Text>
+                  <Button
+                    variant="primary"
+                    onClick={() => updateProduct(item.id)}
+                  >
+                    Details
+                  </Button>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))
+        ) : (
+          <Col>
+            <h1 className="no-results">No Results Found</h1>
+          </Col>
+        )}
+      </Row>
+    </Container>
+  );
+};
+
+export default Products;
